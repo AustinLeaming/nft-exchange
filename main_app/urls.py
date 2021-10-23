2884e699d9f4c3daf499b8a214b2d1a8ae250844
AustinLeaming <austin@Austins-Macbook-Air.local>
2021-10-23 14:22:25 -0600

    created home link in navbar

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('about/', views.about, name='about')
+    path('home/', views.home, name='home'),
+    path('about/', views.about, name='about'),
 ]
