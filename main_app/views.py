0739ed2b4eb54121ea172d0f2602820d936dd53d
AustinLeaming <austin@Austins-Macbook-Air.local>
2021-10-23 14:09:10 -0600

    created base.html template, added base html

--- main_app/views.py
+++ main_app/views.py
@@ -5,3 +5,7 @@ from django.http import HttpResponse
 
 def home(request):
     return HttpResponse('<h1>HOME PAGE</h1>')
+    # ^ change to render when home page design comes in
+
+def about(request):
+    return render(request, 'about.html')
