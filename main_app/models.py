5081259cd93ef09f071c0d12e285fe485ca7c62d
AustinLeaming <austin@Austins-Macbook-Air.local>
2021-10-25 12:12:22 -0600

    moved the nft foreign key into the comment class, removed the f in front of file

--- main_app/models.py
+++ main_app/models.py
@@ -17,7 +17,7 @@ class Nft(models.Model):
     title = models.CharField(max_length=100)
     email = models.CharField(max_length=100)
     price = models.IntegerField()
-    ffile = models.ImageField()
+    file = models.ImageField()
     # user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
@@ -34,8 +34,6 @@ class Comment(models.Model):
         choices=RATINGS,
         default=RATINGS[0][0]
     )
-
-
-nft = models.ForeignKey(Nft, on_delete=models.CASCADE)
+    nft = models.ForeignKey(Nft, on_delete=models.CASCADE)
 
 
--- main_app/urls.py
+++ main_app/urls.py
@@ -4,7 +4,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('nfts/', views.nfts_index, name='index'),
     path('about/', views.about, name='about'),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('nfts/', views.nfts_index, name='index'),
 ]
