27955db25fab107f264086984fe6ddb664605e93
AustinLeaming <austin@Austins-Macbook-Air.local>
2021-10-27 12:27:34 -0600

    added form for photo in detail page

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Nft, Comment
+from .models import Nft, Comment, Photo
 
 admin.site.register(Nft)
 admin.site.register(Comment)
+admin.site.register(Photo)
